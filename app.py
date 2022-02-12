43890432ef36d238177e648a57359b9fefff25d0
Vijay Neethipudi <60729438+vijayneethipudi@users.noreply.github.com>
2022-02-12 12:33:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Web App with Python Flask!'
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
