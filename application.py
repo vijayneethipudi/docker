e22558ff9632f13c7a40b3e94068331e60b876dd
vijayneethipudi <vijay42143@gmail.com>
2022-02-12 22:13:00 +0530

    version change

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return "Hello World! v2"
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run(debug=True)
